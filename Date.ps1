cbf5895fbd87715f68640eab5e7363280b66bc55
Markus Gehrig <markus.gehrig96@gmail.com>
2019-08-31 14:53:32 +0200

    Update Date.ps1

--- Date.ps1
+++ Date.ps1
@@ -1,2 +1,9 @@
-# Montag, 1. April 1996 00:00:00
+# Ausgabe
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-6#notes
+
+# Geburtstag, Montag, 1. April 1996 00:00:00
 Get-Date -Year 1996 -Month 04 -Date 24
+
+# Aktuelles Datum weniger 24 Stunden
+$test = Get-Date
+$test.AddHours(-24)
